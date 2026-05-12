f141cd78a8412e164ed4280aae6b4a718f910fca
kishukumar07 <kishukumars082@gmail.com>
2026-05-12 10:59:31 +0530

    Reapply "modify: nav Archi."
    This reverts commit 84d99f0c431f520c04fa7e404b2e94338bed3e81.

--- components/navbar/NavRight.jsx
+++ components/navbar/NavRight.jsx
@@ -2,35 +2,41 @@ import React from "react";
 import ButtonGroup from "./ButtonGroup";
 import ActionIcon from "./ActionIcon";
 import AvatarButton from "./AvatarButton";
-
+import SearchBox from "./SearchBox";
 import {
+  Bot,
   Plus,
   ChevronDown,
-  LayoutGrid ,
+  LayoutGrid,
   Languages,
   Activity,
   Bell,
 } from "lucide-react";
 
 function NavRight() {
-  // Usage ...
-  // ButtonGroup
-  // ActionIcon
-  // AvatarButton
+ 
 
   return (
     <div className="nav-right">
+   
+        <SearchBox />
+        <ButtonGroup>
+          <ActionIcon icon={<Bot />} title="AI Assistant" />
+          <ActionIcon icon={<ChevronDown />} title="AI Options" />
+        </ButtonGroup>
+     
+
       <ButtonGroup>
-        <ActionIcon icon={<Plus />} title="Open Menu" />{" "}
-        <ActionIcon icon={<ChevronDown />} title="Open Menu" />{" "}
+        <ActionIcon icon={<Plus />} title="Crete new..." />{" "}
+        <ActionIcon icon={<ChevronDown />} title="Create options" />{" "}
       </ButtonGroup>
       <div className="nav-divider"></div>
-      <ActionIcon icon={<LayoutGrid  />} title="Open Menu" />
-      <ActionIcon icon={<Languages />} title="Open Menu" />
-      <ActionIcon icon={<Activity />} title="Open Menu" />
+      <ActionIcon icon={<LayoutGrid />} title="Install App" />
+      <ActionIcon icon={<Languages />} title="Language" />
+      <ActionIcon icon={<Activity />} title="Notifications" />
 
       <div className="nav-divider"></div>
-      <ActionIcon icon={<Bell />} title="Open Menu" />
+      <ActionIcon icon={<Bell />} title="Open user navigation Menu" />
       <AvatarButton />
     </div>
   );
